e6de80218e4bc139ea732e3cb4d15bb97b718ec9
hua暖 <huanuan@xiepingMacBook-Air.local>
2016-08-04 14:34:45 +0800

    step 6 order by wage

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -25,12 +25,13 @@ class JobsController < ApplicationController
 
   def index
     @jobs = case params[:order]
-    when 'by_lower_bound'
-      Job.published.order('wage_lower_bound DESC')
-    when 'by_upper_bound'
-      Job.published.order('wage_upper_bound DESC')
-    else
-      Job.published.recent
+      when 'by_lower_bound'
+        Job.published.order('wage_lower_bound DESC')
+      when 'by_upper_bound'
+        Job.published.order('wage_upper_bound DESC')
+      else
+        Job.published.recent
+      end
   end
 
   def edit
@@ -59,4 +60,5 @@ class JobsController < ApplicationController
   def job_params
    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :resumes
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   def admin?
     email == 'xieping7006@gmail.com'
   end
-  
+
 end
